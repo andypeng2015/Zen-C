918e3b0aa1990d662cb7631c7fc47b57ba2db439
github-action-benchmark <github@users.noreply.github.com>
2026-04-29 06:02:47 +0000

    add Zen Compiler Benchmarks (customSmallerIsBetter) benchmark result for 8bd4b68ccec1e2a3bc2fd7b2646829730b29b531

---
+++ dev/bench/data.js
@@ -0,0 +1,57 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777442566839,
+  "repoUrl": "https://github.com/andypeng2015/Zen-C",
+  "entries": {
+    "Zen Compiler Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "committer": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "distinct": true,
+          "id": "8bd4b68ccec1e2a3bc2fd7b2646829730b29b531",
+          "message": "zenc: fix action.",
+          "timestamp": "2026-04-28T17:23:47+01:00",
+          "tree_id": "e14127e434fa265bcecb089be1c29e74184d103f",
+          "url": "https://github.com/andypeng2015/Zen-C/commit/8bd4b68ccec1e2a3bc2fd7b2646829730b29b531"
+        },
+        "date": 1777442558203,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compiler (Full Suite Transpilation)",
+            "value": 13454,
+            "unit": "ms"
+          },
+          {
+            "name": "Compiler (Avg per file)",
+            "value": 38,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (hello)",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (sha256)",
+            "value": 50,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (sort_large)",
+            "value": 72,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
