8d92b907c4d41c1404f08d877a7e7aec042dda92
github-action-benchmark <github@users.noreply.github.com>
2026-05-20 06:32:46 +0000

    add Zen Compiler Benchmarks (customSmallerIsBetter) benchmark result for e0d845f113295f2066d10c78f2106dcb1d0294cd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1779191326862,
+  "lastUpdate": 1779258766623,
   "repoUrl": "https://github.com/andypeng2015/Zen-C",
   "entries": {
     "Zen Compiler Benchmarks": [
@@ -244,6 +244,55 @@ window.BENCHMARK_DATA = {
           {
             "name": "Runtime (test_sort_large)",
             "value": 66,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "committer": {
+            "email": "zuhaitz.zechhub@gmail.com",
+            "name": "Zuhaitz-dev",
+            "username": "Zuhaitz-dev"
+          },
+          "distinct": true,
+          "id": "e0d845f113295f2066d10c78f2106dcb1d0294cd",
+          "message": "zenc: Windows interop fixes.",
+          "timestamp": "2026-05-19T12:56:58+01:00",
+          "tree_id": "01337a24b3cd7947b8752b9cc9fa255784092f14",
+          "url": "https://github.com/andypeng2015/Zen-C/commit/e0d845f113295f2066d10c78f2106dcb1d0294cd"
+        },
+        "date": 1779258764213,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Compiler (Full Suite Transpilation)",
+            "value": 11617,
+            "unit": "ms"
+          },
+          {
+            "name": "Compiler (Avg per file)",
+            "value": 26,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (test_hello)",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (test_sha256)",
+            "value": 94,
+            "unit": "ms"
+          },
+          {
+            "name": "Runtime (test_sort_large)",
+            "value": 57,
             "unit": "ms"
           }
         ]
